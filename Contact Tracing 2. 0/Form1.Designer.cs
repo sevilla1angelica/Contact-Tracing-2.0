423a55898e79813bb76e90a07917a06b5d36cfa7
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-06-24 23:35:10 +0800

    Added Labels and textboxes for Persoanl Info and Address.

--- Contact Tracing 2. 0/Form1.Designer.cs
+++ Contact Tracing 2. 0/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Contact_Tracing_2._0
 {
-    partial class Form1
+    partial class CntctTrcngform
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CntctTrcngform));
+            this.lblContactTracingForm = new System.Windows.Forms.Label();
+            this.logopic = new System.Windows.Forms.PictureBox();
+            this.btnInformation = new System.Windows.Forms.Button();
+            this.lblPersonalInfo = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtbxFirstName = new System.Windows.Forms.TextBox();
+            this.txtbxLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtbxContactNo = new System.Windows.Forms.TextBox();
+            this.lblContactNo = new System.Windows.Forms.Label();
+            this.txtbxBirthdate = new System.Windows.Forms.TextBox();
+            this.lblBirthdate = new System.Windows.Forms.Label();
+            this.txtbxEmailaddress = new System.Windows.Forms.TextBox();
+            this.lblEmailAddress = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtbxBarangay = new System.Windows.Forms.TextBox();
+            this.lblBarangay = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblMunicipality = new System.Windows.Forms.Label();
+            this.txtbxProvince = new System.Windows.Forms.TextBox();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.txtbxRegion = new System.Windows.Forms.TextBox();
+            this.lblRegion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logopic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblContactTracingForm
+            // 
+            this.lblContactTracingForm.AutoSize = true;
+            this.lblContactTracingForm.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.lblContactTracingForm.Location = new System.Drawing.Point(129, 68);
+            this.lblContactTracingForm.Name = "lblContactTracingForm";
+            this.lblContactTracingForm.Size = new System.Drawing.Size(276, 30);
+            this.lblContactTracingForm.TabIndex = 0;
+            this.lblContactTracingForm.Text = "CONTACT TRACING FORM";
+            this.lblContactTracingForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // logopic
+            // 
+            this.logopic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logopic.ErrorImage = null;
+            this.logopic.Image = global::Contact_Tracing_2._0.Properties.Resources._2_stop_coronavirus_cartoon_clipart;
+            this.logopic.Location = new System.Drawing.Point(12, 41);
+            this.logopic.Name = "logopic";
+            this.logopic.Size = new System.Drawing.Size(80, 80);
+            this.logopic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logopic.TabIndex = 1;
+            this.logopic.TabStop = false;
+            // 
+            // btnInformation
+            // 
+            this.btnInformation.BackColor = System.Drawing.Color.Beige;
+            this.btnInformation.Location = new System.Drawing.Point(8, 12);
+            this.btnInformation.Name = "btnInformation";
+            this.btnInformation.Size = new System.Drawing.Size(92, 23);
+            this.btnInformation.TabIndex = 2;
+            this.btnInformation.Text = "Information";
+            this.btnInformation.UseVisualStyleBackColor = false;
+            // 
+            // lblPersonalInfo
+            // 
+            this.lblPersonalInfo.AutoSize = true;
+            this.lblPersonalInfo.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.lblPersonalInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPersonalInfo.Location = new System.Drawing.Point(12, 149);
+            this.lblPersonalInfo.Name = "lblPersonalInfo";
+            this.lblPersonalInfo.Size = new System.Drawing.Size(138, 19);
+            this.lblPersonalInfo.TabIndex = 3;
+            this.lblPersonalInfo.Text = "Personal Information";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFirstName.Location = new System.Drawing.Point(27, 182);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(64, 13);
+            this.lblFirstName.TabIndex = 5;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // txtbxFirstName
+            // 
+            this.txtbxFirstName.BackColor = System.Drawing.Color.Beige;
+            this.txtbxFirstName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxFirstName.Location = new System.Drawing.Point(182, 179);
+            this.txtbxFirstName.Name = "txtbxFirstName";
+            this.txtbxFirstName.Size = new System.Drawing.Size(223, 22);
+            this.txtbxFirstName.TabIndex = 6;
+            // 
+            // txtbxLastName
+            // 
+            this.txtbxLastName.BackColor = System.Drawing.Color.Beige;
+            this.txtbxLastName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxLastName.Location = new System.Drawing.Point(182, 210);
+            this.txtbxLastName.Name = "txtbxLastName";
+            this.txtbxLastName.Size = new System.Drawing.Size(223, 22);
+            this.txtbxLastName.TabIndex = 8;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLastName.Location = new System.Drawing.Point(27, 213);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(62, 13);
+            this.lblLastName.TabIndex = 7;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtbxContactNo
+            // 
+            this.txtbxContactNo.BackColor = System.Drawing.Color.Beige;
+            this.txtbxContactNo.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxContactNo.Location = new System.Drawing.Point(182, 272);
+            this.txtbxContactNo.Name = "txtbxContactNo";
+            this.txtbxContactNo.Size = new System.Drawing.Size(223, 22);
+            this.txtbxContactNo.TabIndex = 12;
+            // 
+            // lblContactNo
+            // 
+            this.lblContactNo.AutoSize = true;
+            this.lblContactNo.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContactNo.Location = new System.Drawing.Point(27, 275);
+            this.lblContactNo.Name = "lblContactNo";
+            this.lblContactNo.Size = new System.Drawing.Size(74, 13);
+            this.lblContactNo.TabIndex = 11;
+            this.lblContactNo.Text = "Contact No. :";
+            // 
+            // txtbxBirthdate
+            // 
+            this.txtbxBirthdate.BackColor = System.Drawing.Color.Beige;
+            this.txtbxBirthdate.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxBirthdate.Location = new System.Drawing.Point(182, 241);
+            this.txtbxBirthdate.Name = "txtbxBirthdate";
+            this.txtbxBirthdate.Size = new System.Drawing.Size(223, 22);
+            this.txtbxBirthdate.TabIndex = 10;
+            // 
+            // lblBirthdate
+            // 
+            this.lblBirthdate.AutoSize = true;
+            this.lblBirthdate.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBirthdate.Location = new System.Drawing.Point(27, 244);
+            this.lblBirthdate.Name = "lblBirthdate";
+            this.lblBirthdate.Size = new System.Drawing.Size(120, 13);
+            this.lblBirthdate.TabIndex = 9;
+            this.lblBirthdate.Text = "Birthdate (MM/DD/YY):";
+            // 
+            // txtbxEmailaddress
+            // 
+            this.txtbxEmailaddress.BackColor = System.Drawing.Color.Beige;
+            this.txtbxEmailaddress.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxEmailaddress.Location = new System.Drawing.Point(182, 303);
+            this.txtbxEmailaddress.Name = "txtbxEmailaddress";
+            this.txtbxEmailaddress.Size = new System.Drawing.Size(223, 22);
+            this.txtbxEmailaddress.TabIndex = 14;
+            // 
+            // lblEmailAddress
+            // 
+            this.lblEmailAddress.AutoSize = true;
+            this.lblEmailAddress.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEmailAddress.Location = new System.Drawing.Point(27, 306);
+            this.lblEmailAddress.Name = "lblEmailAddress";
+            this.lblEmailAddress.Size = new System.Drawing.Size(83, 13);
+            this.lblEmailAddress.TabIndex = 13;
+            this.lblEmailAddress.Text = "Email address :";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.lblAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAddress.Location = new System.Drawing.Point(12, 335);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(59, 19);
+            this.lblAddress.TabIndex = 15;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtbxBarangay
+            // 
+            this.txtbxBarangay.BackColor = System.Drawing.Color.Beige;
+            this.txtbxBarangay.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxBarangay.Location = new System.Drawing.Point(182, 459);
+            this.txtbxBarangay.Name = "txtbxBarangay";
+            this.txtbxBarangay.Size = new System.Drawing.Size(223, 22);
+            this.txtbxBarangay.TabIndex = 23;
+            // 
+            // lblBarangay
+            // 
+            this.lblBarangay.AutoSize = true;
+            this.lblBarangay.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBarangay.Location = new System.Drawing.Point(27, 462);
+            this.lblBarangay.Name = "lblBarangay";
+            this.lblBarangay.Size = new System.Drawing.Size(60, 13);
+            this.lblBarangay.TabIndex = 22;
+            this.lblBarangay.Text = "Barangay :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Beige;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(182, 428);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 22);
+            this.textBox2.TabIndex = 21;
+            // 
+            // lblMunicipality
+            // 
+            this.lblMunicipality.AutoSize = true;
+            this.lblMunicipality.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMunicipality.Location = new System.Drawing.Point(27, 431);
+            this.lblMunicipality.Name = "lblMunicipality";
+            this.lblMunicipality.Size = new System.Drawing.Size(73, 13);
+            this.lblMunicipality.TabIndex = 20;
+            this.lblMunicipality.Text = "Municipality:";
+            // 
+            // txtbxProvince
+            // 
+            this.txtbxProvince.BackColor = System.Drawing.Color.Beige;
+            this.txtbxProvince.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxProvince.Location = new System.Drawing.Point(182, 397);
+            this.txtbxProvince.Name = "txtbxProvince";
+            this.txtbxProvince.Size = new System.Drawing.Size(223, 22);
+            this.txtbxProvince.TabIndex = 19;
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblProvince.Location = new System.Drawing.Point(27, 400);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(53, 13);
+            this.lblProvince.TabIndex = 18;
+            this.lblProvince.Text = "Province:";
+            // 
+            // txtbxRegion
+            // 
+            this.txtbxRegion.BackColor = System.Drawing.Color.Beige;
+            this.txtbxRegion.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxRegion.Location = new System.Drawing.Point(182, 366);
+            this.txtbxRegion.Name = "txtbxRegion";
+            this.txtbxRegion.Size = new System.Drawing.Size(223, 22);
+            this.txtbxRegion.TabIndex = 17;
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblRegion.Location = new System.Drawing.Point(27, 369);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(47, 13);
+            this.lblRegion.TabIndex = 16;
+            this.lblRegion.Text = "Region:";
+            // 
+            // CntctTrcngform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(508, 709);
+            this.Controls.Add(this.txtbxBarangay);
+            this.Controls.Add(this.lblBarangay);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblMunicipality);
+            this.Controls.Add(this.txtbxProvince);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.txtbxRegion);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtbxEmailaddress);
+            this.Controls.Add(this.lblEmailAddress);
+            this.Controls.Add(this.txtbxContactNo);
+            this.Controls.Add(this.lblContactNo);
+            this.Controls.Add(this.txtbxBirthdate);
+            this.Controls.Add(this.lblBirthdate);
+            this.Controls.Add(this.txtbxLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtbxFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblPersonalInfo);
+            this.Controls.Add(this.btnInformation);
+            this.Controls.Add(this.logopic);
+            this.Controls.Add(this.lblContactTracingForm);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CntctTrcngform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contact Tracing";
+            this.Load += new System.EventHandler(this.CntctTrcngform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logopic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lblContactTracingForm;
+        private PictureBox logopic;
+        private Button btnInformation;
+        private Label lblPersonalInfo;
+        private Label lblFirstName;
+        private TextBox txtbxFirstName;
+        private TextBox txtbxLastName;
+        private Label lblLastName;
+        private TextBox txtbxContactNo;
+        private Label lblContactNo;
+        private TextBox txtbxBirthdate;
+        private Label lblBirthdate;
+        private TextBox txtbxEmailaddress;
+        private Label lblEmailAddress;
+        private Label lblAddress;
+        private TextBox txtbxBarangay;
+        private Label lblBarangay;
+        private TextBox textBox2;
+        private Label lblMunicipality;
+        private TextBox txtbxProvince;
+        private Label lblProvince;
+        private TextBox txtbxRegion;
+        private Label lblRegion;
     }
 }
--- Contact Tracing 2. 0/Form1.cs
+++ Contact Tracing 2. 0/Form1.cs
@@ -1,10 +1,15 @@
 namespace Contact_Tracing_2._0
 {
-    public partial class Form1 : Form
+    public partial class CntctTrcngform : Form
     {
-        public Form1()
+        public CntctTrcngform()
         {
             InitializeComponent();
+        }
+
+        private void CntctTrcngform_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
