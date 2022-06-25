9b312fb84e3d2ce6e8c1187e5f4c155c99d0235a
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-06-26 01:03:47 +0800

    The Supporting is done, hopefully it works on the contact tracing form

--- Contact Tracing 2. 0/SupporterFile.cs
+++ Contact Tracing 2. 0/SupporterFile.cs
@@ -10,5 +10,42 @@ namespace Contact_Tracing_2._0
 {
     internal class SupporterFile
     {
+        private static DataTable Contact_Tracing_Monitory(string location, char delimiter = ',')
+        {
+            DataTable result;
+            string[] linearray = File.ReadAllLines(location);
+            result = FromDataTable(linearray, delimiter);
+            return result;
+        }
+
+        private static DataTable FromDataTable(string[] linearray, char delimiter)
+        {
+            DataTable dt = new DataTable();
+            AddColumnToTable(linearray, delimiter, ref dt);
+            AddRowToTable(linearray, delimiter, ref dt);
+            return dt;
+        }
+
+        private static void AddRowToTable(string[] valueCollection, char delimiter, ref DataTable dt)
+        {
+            for (int i = 0; 1 < valueCollection.Length; i++)
+            {
+                string[] values = valueCollection[i].Split(delimiter);
+                DataRow row = dt.NewRow();
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j];
+                }
+            }
+        }
+        private static void AddColumnToTable(string[] columnCollection, char delimiter, ref DataTable dt)
+        {
+            string[] columns = columnCollection[0].Split(delimiter);
+            foreach (string columnName in columns)
+            {
+                DataColumn dc = new DataColumn(columnName, typeof(string));
+                dt.Columns.Add(dc);
+            }
+        }
     }
 }
